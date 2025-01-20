a1e9605c28ee9294cb5c8516d712e92e20a2dab3
Igor Marques <igor.marques.121@ufrn.edu.br>
2025-01-19 21:41:19 -0300

    Create clinic endpoints related to professionals

---
+++ src/main/java/web2/dev/backpsiplanner/controller/ClinicController.java
@@ -0,0 +1,39 @@
+package web2.dev.backpsiplanner.controller;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import web2.dev.backpsiplanner.dto.ProfessionalInfoDTO;
+import web2.dev.backpsiplanner.service.ClinicService;
+import web2.dev.backpsiplanner.service.ProfessionalService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clinic")
+@SecurityRequirement(name = "Bearer Authentication")
+public class ClinicController {
+    private final ClinicService clinicService;
+    private final ProfessionalService professionalService;
+
+    public ClinicController(ClinicService clinicService, ProfessionalService professionalService) {
+        this.clinicService = clinicService;
+        this.professionalService = professionalService;
+    }
+
+    @GetMapping("/professionals/available/")
+    public List<ProfessionalInfoDTO> findAvailableProfessionals() {
+        return professionalService.getAllAutonomousProfessionals();
+    }
+
+    @PostMapping("/professionals/add")
+    public ResponseEntity<String> addProfessional(@RequestBody ProfessionalInfoDTO professionalInfoDTO, @RequestParam Long clinicId) {
+        clinicService.addProfessional(professionalInfoDTO, clinicId);
+        return ResponseEntity.ok("Successfully added the professional to the clinic");
+    }
+
+    @GetMapping("/professionals/list")
+    public List<ProfessionalInfoDTO> listProfessionals(@RequestParam Long clinicId) {
+        return professionalService.getAllByClinicId(clinicId);
+    }
+}
--- src/main/java/web2/dev/backpsiplanner/service/ClinicService.java
+++ src/main/java/web2/dev/backpsiplanner/service/ClinicService.java
@@ -2,7 +2,11 @@ package web2.dev.backpsiplanner.service;
 
 import org.springframework.stereotype.Service;
 import web2.dev.backpsiplanner.dto.ClinicInfoDTO;
+import web2.dev.backpsiplanner.dto.ProfessionalInfoDTO;
+import web2.dev.backpsiplanner.model.Clinic;
+import web2.dev.backpsiplanner.model.Professional;
 import web2.dev.backpsiplanner.repository.ClinicRepository;
+import web2.dev.backpsiplanner.repository.ProfessionalRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,9 +14,11 @@ import java.util.stream.Collectors;
 @Service
 public class ClinicService {
     private final ClinicRepository clinicRepository;
+    private final ProfessionalRepository professionalRepository;
 
-    public ClinicService(ClinicRepository clinicRepository) {
+    public ClinicService(ClinicRepository clinicRepository, ProfessionalRepository professionalRepository) {
         this.clinicRepository = clinicRepository;
+        this.professionalRepository = professionalRepository;
     }
 
     public List<ClinicInfoDTO> getAllNotEmptyClinics() {
@@ -31,5 +37,20 @@ public class ClinicService {
         return clinicRepository.findByProfessionalsIsNotEmptyAndLocationContainingIgnoreCase(location).stream()
                 .map(clinic -> new ClinicInfoDTO().from(clinic))
                 .collect(Collectors.toList());
+    }
+
+    public void addProfessional(ProfessionalInfoDTO professionalInfoDTO, Long clinicId) {
+        Professional professional = professionalRepository.findById(professionalInfoDTO.getId()).orElse(null);
+        Clinic clinic = clinicRepository.findById(clinicId).orElse(null);
+
+        if (clinic == null)
+            throw new RuntimeException("Clinic not found");
+        if (professional == null)
+            throw new RuntimeException("Professional not found");
+        if (professional.getClinic() != null)
+            throw new RuntimeException("Professional already registered in another clinic: " + professional.getClinic().getName());
+
+        professional.setClinic(clinic);
+        professionalRepository.save(professional);
     }
 }
